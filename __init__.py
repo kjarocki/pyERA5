651800351b2529e219fe95ea20bbab046854bbcc
Konrad <konradjarocki@gmail.com>
2022-03-07 12:47:02 +0100

    initial comits for idea

---
+++ __init__.py
@@ -0,0 +1,4 @@
+
+cds_url: str = "https://cds.climate.copernicus.eu/api/v2"
+cds_uid: str = "95488"
+cds_key: str = "c4c2e067-74e7-42f4-a8fe-5cb60dc8ef6f"
---
+++ main.py
@@ -0,0 +1,13 @@
+
+### Download ERA5 data for given index and given shape (
+    # in: index_name, dates, shape in WKT
+### Computer average over the shape (any given shape)
+### Time series (save to csv)
+### Anomalies (save to csv)
+### Thresholds (quantiles)
+### Visualisation – plot (on screen, to file)
+### Visualization – day on the map (or average over period)
+### Visualization – TS gif on map
+### Season detection
+### Embedded in django or flask
+### Correliation analysis for given input
